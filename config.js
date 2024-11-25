2f23bd43d2a5afe97b5d6425a3ca666b4832dbba
tasosstot <70763683+tasosstot@users.noreply.github.com>
2024-11-25 15:04:10 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const config = {
   // Talk to @BotFather to set up a bot and get the token
-  token: "5811729088:AAE0LzeSS_xuS2O-GF1PpS__YRClF9MWS5o",
-  chatId: "901354561",
+  token: "",
+  chatId: "",
   // Define feed fetching interval in seconds
   refreshInterval: 5,
 };
